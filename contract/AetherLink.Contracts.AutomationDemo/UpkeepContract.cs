fb8bf200099138ed13c61f4579aad5f639f10cb6
so000up <dragon@aetherlink.network>
2024-09-11 14:34:20 +0800

    init demo contract

---
+++ contract/AetherLink.Contracts.AutomationDemo/UpkeepContract.cs
@@ -0,0 +1,5 @@
+namespace AetherLink.Contracts.AutomationDemo;
+
+public class UpkeepContract
+{
+}
---
+++ contract/AetherLink.Contracts.AutomationDemo/UpkeepContractState.cs
@@ -0,0 +1,7 @@
+using AElf.Sdk.CSharp.State;
+
+namespace AetherLink.Contracts.AutomationDemo;
+
+public class UpkeepContractState : ContractState
+{
+}
